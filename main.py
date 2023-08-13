ffb588030004fe5c6d445cb70b3c0973652dc46f
Sohel Datta <soheldatta17@gmail.com>
2023-08-13 12:30:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-Example 1:
-
-Input: N = 6
-arr = {1, 2, 3, 6, 5, 4}
-Output: {1, 2, 4, 3, 5, 6}
-Explaination: The next permutation of the 
-given array is {1, 2, 4, 3, 5, 6}.
+class Solution:
+  
+    def nextPermutation(self, N, arr):
+      
+        if N==0:
+            return
+        if N==1:
+            return arr
+        k=N-2
+        for i in range(N-1,0,-1):
+            if arr[i]<=arr[i-1]:
+                k-=1
+            else:
+                break
+        if k==-1:
+            return arr[::-1]
+            
+        for i in range(N-1,0,-1):
+            if arr[i]>arr[k]:
+                arr[i],arr[k]=arr[k],arr[i]
+                break
+        return arr[0:k+1]+arr[:k:-1]
